807c54f3398704a251ab73012d994f36b3a0f1ab
Hong Jing (Jingles) <jinglescode@gmail.com>
2023-07-30 00:14:41 +0800

    test splash screen

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withPWA = require("next-pwa")({
   dest: "public",
-  // disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
 });
 
 module.exports = withPWA({ reactStrictMode: true });
