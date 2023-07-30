cb27f6f5750f57c13f84387f479e912787581dc1
Hong Jing (Jingles) <jinglescode@gmail.com>
2023-07-30 22:17:16 +0800

    test remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-const withPWA = require("next-pwa")({
-  dest: "public",
-  // register: true,
-  disable: process.env.NODE_ENV === "development",
-});
+// const withPWA = require("next-pwa")({
+//   dest: "public",
+//   register: true,
+//   disable: process.env.NODE_ENV === "development",
+// });
 
-module.exports = withPWA({ reactStrictMode: true });
+// module.exports = withPWA({ reactStrictMode: true });
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+module.exports = nextConfig;
