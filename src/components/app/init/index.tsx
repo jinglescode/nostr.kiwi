f38e7b2034f314c2545e0ce7abb76341ebf1a9b1
Hong Jing (Jingles) <jinglescode@gmail.com>
2023-07-31 13:13:28 +0800

    iosDynamicNavbar

--- src/components/app/init/index.tsx
+++ src/components/app/init/index.tsx
@@ -58,7 +58,7 @@ export default function AppInit({
               <>
                 <View
                   main
-                  iosDynamicNavbar={false}
+                  iosDynamicNavbar={true}
                   // xhrCache={false}
                   // browserHistory
                   // browserHistorySeparator=""
